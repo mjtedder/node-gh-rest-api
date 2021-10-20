7661911bd72a594c0c6b4a7445d47f1dff7e2f4b
Matt Tedder <mjtedder@icloud.com>
2021-10-20 11:58:43 -0600

    Adds wildcard route which defaults to home page.  Adds api route for fetching PR data.

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,20 @@
 const path = require('path');
+const { getOpenPRData } = require('../models');
 
 module.exports = app => {
+
+
+    app.get('/pull-requests', async(req, res, next) => {
+        try {
+            res.json({
+                data: await getOpenPRData(req)
+            });
+        } catch (err) {
+            next(err);
+        }
+    });
+
     app.get("*", (req, res) => {
-        return res.send('Hello World');
+        return res.sendFile(path.join(__dirname, '../public/index.html'));
     });
 };
