86fc7649a7e804c1f5c5b4f16c676ae2f584d44f
Matt Tedder <mjtedder@icloud.com>
2021-10-20 12:01:57 -0600

    Adds models folder, and a method for fetching PR Data

---
+++ models/index.js
@@ -0,0 +1,24 @@
+const axios = require('axios');
+require('dotenv').config();
+
+const { baseUrl, headers } = require('../constants'); 
+const db = require('../db/db.json');
+const { retrievePRNumbers } = require('../utils');
+
+
+const getOpenPRData = async () => {
+    try {
+        const response = await axios.get(baseUrl, headers);
+        const openPullRequests = response.data.filter(pr => pr.state);
+        if (openPullRequests.length) {
+            db.prNumbers = retrievePRNumbers(openPullRequests);
+        }
+        return openPullRequests;
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+module.exports = {
+    getOpenPRData
+}
